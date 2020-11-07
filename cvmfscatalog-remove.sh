05754efb8f99da76a23311810db7a150a8a5ea8e
Wouter Deconinck <wdconinc@jlab.org>
2020-11-06 19:19:45 -0500

    [release] do not remove catalog when release pending

--- cvmfscatalog-remove.sh
+++ cvmfscatalog-remove.sh
@@ -2,6 +2,9 @@
 
 cvmfs=${1:-/cvmfs/eic.opensciencegrid.org/packages}
 
+# Do not remove catalogs when release pending
+if [ -f ${cvmfs}/CVMFSRELEASE ] ; then exit ; fi
+
 # Remove cvmfscatalog
 find ${cvmfs} -mindepth 4 -maxdepth 4 -name .cvmfscatalog -delete
 find ${cvmfs}/spack/current/var/spack/environments/ -maxdepth 4 -name .cvmfscatalog -delete
