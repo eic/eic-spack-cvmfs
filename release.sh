69c23c9dd4c619711ac68962f9a8e3cee1292f0e
Wouter Deconinck <wdconinc@jlab.org>
2020-11-06 19:20:33 -0500

    [release] use cmvfs=$1; no garbage collect

--- release.sh
+++ release.sh
@@ -2,19 +2,21 @@
 
 dir=`dirname ${0}`
 
+cvmfs=${1:-/cvmfs/eic.opensciencegrid.org/packages}
+
 # Group umask
 umask 002
 
 # Load spack
-source /cvmfs/eic.opensciencegrid.org/packages/setup-env.sh
+source ${cvmfs}/setup-env.sh
 
 # Clean spack (stage, downloads, failures)
 echo "Cleaning temporary files"
 spack clean -s -d -f
 
 # Garbage collect (disabled to reuse build dependencies)
-echo "Removing unneeded specs"
-spack gc -y
+#echo "Removing unneeded specs"
+#spack gc -y
 
 # Show explicitly installed packages
 echo "Explicitly installed packages:"
@@ -22,10 +24,10 @@ spack find -x -v -L
 
 # Show disk usage
 echo "Disk usage (top 10 packages): [this may take a while]"
-du -sh /cvmfs/eic.opensciencegrid.org/packages/* | sort -h -r | head -n 10
+du -sh ${cvmfs}/* | sort -h -r | head -n 10
 
 # Add cvmfscatalog
-${dir}/cvmfscatalog-add.sh /cvmfs/eic.opensciencegrid.org/packages
+${dir}/cvmfscatalog-add.sh ${cvmfs}
 
 # Release
-touch /cvmfs/eic.opensciencegrid.org/packages/CVMFSRELEASE
+touch ${cvmfs}/CVMFSRELEASE
