c5a77a12a63dea66b306a1d8eec9b909d7efe928
Wouter Deconinck <wdconinc@jlab.org>
2021-03-24 20:07:02 -0400

    [update-all] loop over local OS; TINI_SUBREAPER; log file time stamps

--- update-all.sh
+++ update-all.sh
@@ -29,13 +29,24 @@ fi
 # Exit if no changes
 [[ "${spack_unchanged}" -eq 1 && "${eic_spack_unchanged}" -eq 1 && $# -eq 0 ]] && exit
 
-# Loop over operating systems
+# Loop over local operating systems
+for os in rhel7 ; do
+  if [ -w ${SPACK_ROOT}/../log ] ; then
+    log=$SPACK_ROOT/../log/spack-builder:${os}-$(date --iso-8601=minutes).log
+  else
+    log=/tmp/spack-builder:${os}-$(date --iso-8601=minutes).log
+  fi
+  ${dir}/update.sh 2>&1 | tee ${log}
+done
+
+# Loop over container operating systems
 for os in centos7 centos8 ubuntu18.04 ubuntu20.04 ubuntu20.10 ; do
   if [ -w ${SPACK_ROOT}/../log ] ; then
-    log=$SPACK_ROOT/../log/spack-builder:${os}-$(date --iso-8601).log
+    log=$SPACK_ROOT/../log/spack-builder:${os}-$(date --iso-8601=minutes).log
   else
-    log=/tmp/spack-builder:${os}-$(date --iso-8601).log
+    log=/tmp/spack-builder:${os}-$(date --iso-8601=minutes).log
   fi
+  export TINI_SUBREAPER=""
   singularity run -B /cvmfs:/cvmfs /cvmfs/eic.opensciencegrid.org/singularity/spack-builder:${os} ${dir}/update.sh 2>&1 | tee ${log}
 done
 
